5b08b2e2d20b1f6d93448e2daab80a3e185c0674
Neelesh Sai <rneeleshsai@gmail.com>
2026-02-03 14:56:34 -0700

    change repo name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Tianoshield_WebPage/', // GitHub repo name
+  base: '/home/',//GitHub Repo name
   server: {
     port: 5173,
     open: true
