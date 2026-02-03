42c8ba93a04e7f73c778c824ead9c982661341bb
Neelesh Sai <rneeleshsai@gmail.com>
2026-02-03 12:00:38 -0700

    updated necessary files for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tianoshield-research/', // ⚠️ CHANGE THIS to your GitHub repo name!
+  base: '/Tianoshield_WebPage/', // GitHub repo name
   server: {
     port: 5173,
     open: true
